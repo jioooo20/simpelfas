8ce7bdce47a400564a2805192838c1dcc692a02a
Yeon <giojio825@gmail.com>
2025-05-19 21:32:58 +0700

    feat: Create 't_perbaikan_petugas' migration with foreign key relationships

--- database/migrations/2025_05_15_185846_create_t_perbaikan_table.php
+++ database/migrations/2025_05_15_185846_create_t_perbaikan_table.php
@@ -14,13 +14,11 @@ return new class extends Migration
         Schema::create('t_perbaikan', function (Blueprint $table) {
             $table->id('perbaikan_id');
             $table->unsignedBigInteger('pelaporan_id')->index();
-            $table->unsignedBigInteger('user_id')->index();
             $table->string('perbaikan_kode', 10)->unique();
             $table->string('perbaikan_deskripsi', 255);
             $table->timestamps();
 
             $table->foreign('pelaporan_id')->references('pelaporan_id')->on('m_pelaporan');
-            $table->foreign('user_id')->references('user_id')->on('m_user');
         });
     }
 
