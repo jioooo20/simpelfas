720bd6aa1bd3be012572accf0f7cab8ed713b9a2
Yeon <giojio825@gmail.com>
2025-06-04 20:41:27 +0700

    feat(database): update migration types and add AltSkorSeeder for seeding data

--- database/seeders/PelaporanSeeder.php
+++ database/seeders/PelaporanSeeder.php
@@ -15,7 +15,7 @@ class PelaporanSeeder extends Seeder
     {
         DB::table('m_pelaporan')->insert([
             [
-            'user_id' => 6,
+            'user_id' => 5,
             'fasilitas_id' => 5, //ac
             'pelaporan_kode' => 'PLP-0001',
             'pelaporan_deskripsi' => 'AC tidak berfungsi dengan baik, suhu tidak turun meskipun sudah diatur.',
@@ -23,7 +23,7 @@ class PelaporanSeeder extends Seeder
             'updated_at' => now(),
             ],
             [
-            'user_id' => 4,
+            'user_id' => 5,
             'fasilitas_id' => 1, //meja
             'pelaporan_kode' => 'PLP-0002',
             'pelaporan_deskripsi' => 'meja pecah, tidak bisa digunakan.',
@@ -31,17 +31,25 @@ class PelaporanSeeder extends Seeder
             'updated_at' => now(),
             ],
             [
-            'user_id' => 5,
-            'fasilitas_id' => 2, //proyektor
-            'pelaporan_kode' => 'PLP-0003',
-            'pelaporan_deskripsi' => 'tidak bisa digunakan.',
+            'user_id' => 6,
+            'fasilitas_id' => 1, //meja
+            'pelaporan_kode' => 'PLR0003',
+            'pelaporan_deskripsi' => 'meja pecah, tidak bisa digunakan.',
             'created_at' => now(),
             'updated_at' => now(),
             ],
             [
             'user_id' => 6,
+            'fasilitas_id' => 2, //proyektor
+            'pelaporan_kode' => 'PLR0004',
+            'pelaporan_deskripsi' => 'tidak bisa digunakan.',
+            'created_at' => now(),
+            'updated_at' => now(),
+            ],
+            [
+            'user_id' => 4,
             'fasilitas_id' => 3, //kursi
-            'pelaporan_kode' => 'PLP-0004',
+            'pelaporan_kode' => 'PLR0005',
             'pelaporan_deskripsi' => 'tidak bisa digunakan.',
             'created_at' => now(),
             'updated_at' => now(),
@@ -49,15 +57,7 @@ class PelaporanSeeder extends Seeder
             [
             'user_id' => 4,
             'fasilitas_id' => 4, //papan tulis
-            'pelaporan_kode' => 'PLP-0005',
-            'pelaporan_deskripsi' => 'tidak bisa digunakan.',
-            'created_at' => now(),
-            'updated_at' => now(),
-            ],
-            [
-            'user_id' => 5,
-            'fasilitas_id' => 4, //papan tulis
-            'pelaporan_kode' => 'PLP-0006', // Assuming unique codes, changed from PLR0005
+            'pelaporan_kode' => 'PLR0006',
             'pelaporan_deskripsi' => 'tidak bisa digunakan.',
             'created_at' => now(),
             'updated_at' => now(),
